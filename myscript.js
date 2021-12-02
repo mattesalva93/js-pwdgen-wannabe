7e009c8456acbc9fa8d6ffe9a172a7668f3c0293
mattesalva93 <salvalaggiomatteo93@gmail.com>
2021-12-02 16:03:36 +0100

    all done

---
+++ myscript.js
@@ -0,0 +1,7 @@
+let inputname = prompt("Inserisci il tuo nome")
+
+let inputsurname = prompt("Inserisci il tuo cognome")
+
+let inputfavcolor = prompt("Inserisci il tuo colore preferito")
+
+document.getElementById("pswgen").innerHTML = inputname + inputsurname + inputfavcolor + "21"
